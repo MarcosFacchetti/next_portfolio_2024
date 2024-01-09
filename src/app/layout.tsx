aefe1572163b7c03f5686f113258688333750d68
marcosfacchetti <marcos-facchetti@hotmail.com>
2024-01-08 23:32:50 -0300

    les gouss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from '@/Components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,14 +16,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className} >
+      <body className={inter.className}>
         <main>
+          <nav className="fixed w-full h-10 bg-slate-950 border-b-2 py-10 flex items-center justify-center">
+            <ul className="flex flex-row gap-10">
+              <li>
+                <a href="/">Inicio</a>
+              </li>
+              <li>
+                <a href="/Proyectos">Proyectos</a>
+              </li>
+              <li>
+                <a href="/Contacto">Contacto</a>
+              </li>
+            </ul>
+          </nav>
 
-        <Navbar/>
-        {children}
-
+          {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
