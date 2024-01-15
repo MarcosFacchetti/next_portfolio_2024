be37bf28a0183ed434bada69ebac7f98912f1a9c
marcosfacchetti <marcos-facchetti@hotmail.com>
2024-01-15 10:03:17 -0300

    kik

--- src/app/Proyectos/Sandbox/page.tsx
+++ src/app/Proyectos/Sandbox/page.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import Jebus from "./jebusDance.gif"
-import Image from 'next/image'
+import React from "react";
+import Jebus from "./jebusDance.gif";
+import Image from "next/image";
 
 const Sandbox = () => {
   return (
-    <div>
-      <Image className='mt-40 h-40' src={Jebus} alt='jebus'></Image>
-    </div>
-  )
-}
+    <div className="flex flex-col-reverse h-screen w-full justify-center items-center">
+      <div>
+        <Image className="h-auto" src={Jebus} alt="jebus" />
 
-export default Sandbox
+        <iframe
+          width="240"
+          height="120"
+          src="https://www.youtube.com/embed/7T_YtklLyyo?si=5VY9WxRgcbiGmQMP&autoplay=1&controls=1&loop=1&playlist=7T_YtklLyyo"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          className="fixed rounded-2xl opacity-10 bottom-5 right-5 hover:opacity-100 transition-opacity"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Sandbox;
