4c33d2b0e45dc3c7c6bdf4c61ed8a262f099b58f
marcosfacchetti <marcos-facchetti@hotmail.com>
2024-01-24 09:00:37 -0300

    carrusel con componentes

--- src/app/Proyectos/Tareas/page.tsx
+++ src/app/Proyectos/Tareas/page.tsx
@@ -19,25 +19,34 @@ const Tareas: React.FC = () => {
   };
 
   return (
-    <div className='flex flex-col items-center p-10 '>
-      <h1 className='font-bold text-2xl mt-5 mb-3'>Gestor de tareas</h1>
-      <div className='flex items-center mb-4'>
+    <div className="flex flex-col items-center p-10  w-80">
+      <h1 className="font-bold text-2xl mt-5 mb-3">Gestor de tareas</h1>
+      <div className="flex flex-col items-center mb-4">
+        <button
+          className="bg-blue-950 text-white px-4 py-2 rounded"
+          onClick={agregarTarea}
+        >
+          Agregar
+        </button>
         <input
-          type='text'
-          className='border-2 text-slate-950 p-2 mr-2'
-          placeholder='Agregar tarea'
+          type="text"
+          className="border-2 text-slate-950 p-2 mt-5 "
+          placeholder="Agregar tarea"
           value={nuevaTarea}
           onChange={(e) => setNuevaTarea(e.target.value)}
         />
-        <button className='bg-blue-950 text-white px-4 py-2 rounded' onClick={agregarTarea}>
-          Agregar
-        </button>
       </div>
       <ul className="flex flex-col flex-wrap ">
         {tareas.map((tarea: string, index: number) => (
-          <li key={index} className='flex justify-between items-center border-b p-2'>
+          <li
+            key={index}
+            className="flex justify-between items-center border-b p-2"
+          >
             {tarea}
-            <button className='text-red-500 ml-5' onClick={() => borrarTarea(index)}>
+            <button
+              className="text-red-500 ml-5"
+              onClick={() => borrarTarea(index)}
+            >
               Borrar
             </button>
           </li>
